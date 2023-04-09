5a54f0b61d7a239ef433a672bc5a0f6047dfc500
KovalYevhenii <kovalyevhenii@gmail.com>
2023-04-09 14:23:11 +0200

    firstPart

---
+++ PlanetsAnonTypeApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace PlanetsAnonTypeApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var planetM = new
+            {
+                Name = "Mars",
+                OrdinalNum = 4,
+                EquatorLength = 21.3444
+            };
+            var planetE = new
+            {
+                Name = "The Earth",
+                OrdinalNum = 3,
+                EquatorLength = 40.075,
+                PreviousPlanet = planetM
+            };
+            var planetV = new
+            {
+                Name = "Venus",
+                OrdinalNum = 2,
+                EquatorLength = 380.25,
+                PreviousPlanet = planetE
+            };
+
+            var planetV_2 = new
+            {
+                Name = "Venus",
+                OrdinalNum = 2,
+                EquatorLength = 380.25,
+                PreviousPlanet = planetE
+            };
+            Console.WriteLine($"Planet: {planetM},\n Equivalent to Venus = {planetM.Equals(planetV)}\n");
+            Console.WriteLine("\n");
+            Console.WriteLine($"Planet: {planetE},\n Equivalent to Venus = {planetE.Equals(planetV)}\n");
+            Console.WriteLine("\n");
+            Console.WriteLine($"Planet: {planetV},\n  Equivalent to Venus = {planetV.Equals(planetV_2)}\n");
+            Console.WriteLine("\n");
+
+        }
+    }
+
+
+}
+
