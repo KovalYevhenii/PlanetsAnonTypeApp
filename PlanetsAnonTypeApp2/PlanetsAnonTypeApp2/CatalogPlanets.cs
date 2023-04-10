38558f493ebbd06185b2f5e59a7e1faa9eb826fc
KovalYevhenii <kovalyevhenii@gmail.com>
2023-04-10 11:02:17 +0200

    V2.0

---
+++ PlanetsAnonTypeApp2/PlanetsAnonTypeApp2/CatalogPlanets.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetsAnonTypeApp2
+{
+    public class CatalogOfPlanets
+    {
+       private List<Planet> Planets { get; set; }
+        private int _getPlanetCount;
+
+        public CatalogOfPlanets()
+        {
+            Planets  = new List<Planet>
+            {
+                new Planet ("Venus",2, 380.25),
+                new Planet ("The Earth", 3, 40.075),
+                new Planet ("Mars", 4, 21.3444)
+            };
+            _getPlanetCount = 0;
+        }
+        public void GetPlanet(string planetName)
+        {
+          var planet = Planets.FirstOrDefault(p => p.Name.Equals(planetName, StringComparison.OrdinalIgnoreCase));
+
+            if(_getPlanetCount>= 3)
+            {
+                Console.WriteLine("You have reached the maximum number of calls to the GetPlanet method.");
+                return;
+            }
+            if(planet != null)
+            {
+                Console.WriteLine($"OrdinalNumber : {planet.OrderNumber}");
+                Console.WriteLine($"Equator length: {planet.EquatorLength }");
+            }
+            else
+            {
+                Console.WriteLine($"Planet {planetName} not found");
+            }
+            _getPlanetCount++;
+            
+        }
+    }
+}
---
+++ PlanetsAnonTypeApp2/PlanetsAnonTypeApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace PlanetsAnonTypeApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CatalogOfPlanets catalog = new();
+            catalog.GetPlanet("the earth");
+            Console.WriteLine("--------");
+            catalog.GetPlanet("Lemonia");
+            Console.WriteLine("--------");
+            catalog.GetPlanet("mars");
+            Console.WriteLine("--------");
+            catalog.GetPlanet("mars");
+        }
+    }
+}
